53cca47dda671b39335ddff3497983ff41d7f9b5
Dominik Ružić <ruzicdominik11@gmail.com>
2023-04-04 17:19:59 +0200

    Import TMP

---
+++ Assets/CoinSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinSystem : MonoBehaviour
+{
+    [SerializeField] private int coinAmount = 0;
+    [SerializeField] private TextMeshProUGUI textCoinAmount;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            coinAmount++;
+            textCoinAmount.text = "Total coins" + coinAmount;
+            Destroy(other);
+        }
+    }
+}

2c7e25199d069d83e6aaa24768c88785e4241f72
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2023-06-12 15:33:48 +0200

    AirPortal script fix

--- Assets/Scripts/PortalScripts/AirPortal.cs
+++ Assets/Scripts/PortalScripts/AirPortal.cs
@@ -7,12 +7,6 @@ public class AirPortal : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-         PlayerInventoryForTotem playerInventory = other.GetComponent<PlayerInventoryForTotem>();
-        if(playerInventory.numberOfTotem == 1){
         SceneManager.LoadScene(5);
-        }
-        else {
-            Scene currentScene = SceneManager.GetActiveScene();
-        }
     }
 }
